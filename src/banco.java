b1af77777ddc05ee09a2c0c4ae82fbbea6a7bf09
Vitoria Alvares <236394387+alv-vitoria@users.noreply.github.com>
2026-06-05 17:49:17 -0300

    Refactor Banco class with improved formatting
    Comments add

--- src/banco.java
+++ src/banco.java
@@ -1,25 +1,31 @@
 public class Banco {
-    
+
+    // Guarda a única instância da classe
     private static Banco instancia;
     private String nome;
     private double saldo;
-    
+
+     // Construtor privado para impedir criação direta de objetos
     private Banco() {
         this.nome = "Banco Digital";
         this.saldo = 1000.0;
     }
-    
+
+  // Retorna a única instância do banco
+    // Caso ainda não exista, ela é criada
     public static Banco getInstancia() {
         if (instancia == null) {
             instancia = new Banco();
         }
         return instancia;
     }
-    
+
+     // Adiciona valor ao saldo
     public void depositar(double valor) {
         this.saldo += valor;
     }
-    
+
+        // Realiza saque apenas se houver saldo suficiente
     public void sacar(double valor) {
         if (valor <= saldo) {
             this.saldo -= valor;
@@ -27,7 +33,7 @@ public class Banco {
             System.out.println("Saldo insuficiente.");
         }
     }
-    
+    // Exibe as informações do banco
     public void exibirInfo() {
         System.out.println("Banco: " + nome + " | Saldo: R$" + saldo);
     }
